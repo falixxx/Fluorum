acd160c09c0d334b6021816eb2c1abf4c11f5a0f
falixkamishin <kamilovdanya1337@gmail.com>
2023-04-20 19:53:11 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -8,6 +8,10 @@ if(game.PlaceId == 537413528) then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/falixxx/Fluorum/main/games/BuildABoat.lua", true))()
 end
 
+if(game.PlaceId == 1962086868) then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/falixxx/Fluorum/main/games/TowerOfHell.lua", true))()
+end
+
 if(game.PlaceId == 6516141723) then
   OrionLib:MakeNotification({Name="Notification",Content="Execute in game, not lobby!",Image="rbxassetid://4483345998",Time=5})
 end
