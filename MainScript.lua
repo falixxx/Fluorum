a04d5874a445ad9f6bb39eebb3e7c4e81a21a202
falixkamishin <kamilovdanya1337@gmail.com>
2023-04-20 19:21:45 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -4,6 +4,10 @@ if(game.PlaceId == 6839171747) then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/falixxx/Fluorum/main/games/DOORS.lua", true))()
 end
 
+if(game.PlaceId == 537413528) then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/falixxx/Fluorum/main/games/BuildABoat.lua", true))()
+end
+
 if(game.PlaceId == 6516141723) then
   OrionLib:MakeNotification({Name="Notification",Content="Execute in game, not lobby!",Image="rbxassetid://4483345998",Time=5})
 end
