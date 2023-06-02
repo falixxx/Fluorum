349ec1067e996e36676ef534b10fa696519c332d
falixkamishin <kamilovdanya1337@gmail.com>
2023-06-02 10:39:35 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,31 +1,17 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-if(game.PlaceId == 6839171747) then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/falixxx/Fluorum/main/games/DOORS.lua", true))()
-end
+local places = {
+    [{6839171747}] = "https://raw.githubusercontent.com/falixxx/Fluorum/main/games/DOORS.lua",
+    [{537413528}] = "https://raw.githubusercontent.com/falixxx/Fluorum/main/games/BuildABoat.lua",
+    [{1962086868, 3582763398}] = "https://raw.githubusercontent.com/falixxx/Fluorum/main/games/TowerOfHell.lua",
+    [{189707}] = "https://raw.githubusercontent.com/falixxx/Fluorum/main/games/NDS.lua",
+    [{155615604}] = "https://raw.githubusercontent.com/falixxx/Fluorum/main/games/PrisonLife.lua",
+    [{2537430692}] = "https://raw.githubusercontent.com/falixxx/Fluorum/main/games/Jenga.lua",
+}
 
-if(game.PlaceId == 537413528) then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/falixxx/Fluorum/main/games/BuildABoat.lua", true))()
+for ids, url in next, places do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))()
+        break
+    end
 end
-
-if(game.PlaceId == 1962086868) then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/falixxx/Fluorum/main/games/TowerOfHell.lua", true))()
-end
-
-if(game.PlaceId == 189707) then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/falixxx/Fluorum/main/games/NDS.lua", true))()
-end
-
-if(game.PlaceId == 155615604) then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/falixxx/Fluorum/main/games/PrisonLife.lua", true))()
-end
-
-if(game.PlaceId == 2537430692) then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/falixxx/Fluorum/main/games/Jenga.lua", true))()
-end
-
-if(game.PlaceId == 6516141723) then
-  OrionLib:MakeNotification({Name="Notification",Content="Execute in game, not lobby!",Image="rbxassetid://4483345998",Time=5})
-end
-
-print(nil)
